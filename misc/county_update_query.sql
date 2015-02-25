e2a92bb32717601d3d0b01eb4b0b24077aaa5196
wilsaj <wilson.andrew.j+github@gmail.com>
2015-02-25 14:23:06 -0600

    add query for assigning closest county for remaining quads instead of deleting them

--- misc/county_update_query.sql
+++ misc/county_update_query.sql
@@ -21,5 +21,25 @@ FROM (
 where cartodb_id=c.quad_id
 
 
--- delete county-less quads (no intersection with a county);
-DELETE from tnris_quad_wgs84_copy WHERE closest_county is null
+-- add remaining quads (with no intersection with a county);
+UPDATE tnris_quad_wgs84_copy SET closest_county=c.countyname
+FROM (
+  SELECT quad_id, quadname, countyname, d
+  FROM (SELECT *, rank() OVER (PARTITION BY quadname ORDER BY d ASC) as r
+    FROM (
+      SELECT *, st_distance(st_centroid(q.quadgeom), q.countygeom) as d
+      FROM (
+        SELECT quad.cartodb_id as quad_id, quad.quadname,
+               county.name as countyname,
+               quad.the_geom_webmercator as quadgeom,
+               county.the_geom_webmercator as countygeom
+        FROM county_extended as county, (
+          SELECT * from tnris_quad_wgs84_copy
+          WHERE tnris_quad_wgs84_copy.closest_county is null
+        ) as quad
+      ) as q
+    ) as t
+  ) as z
+  WHERE r = 1
+) as c
+where cartodb_id=c.quad_id
