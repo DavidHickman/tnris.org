635a7b5433057b42c9d480a5b3f8c327fc84fb41
wilsaj <wilson.andrew.j+github@gmail.com>
2015-04-08 14:47:20 -0500

    hide data callout when switching to status map to avoid obfuscating required map attribution info

--- static/js/site.js
+++ static/js/site.js
@@ -77,7 +77,6 @@
     $("a[href^='\#']").each(function(){
       this.href=location.href.split("#")[0]+'#'+this.href.substr(this.href.indexOf('#')+1);
     });
-
   });
 
   $(window).load(function() {
