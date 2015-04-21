bfad2a43c91e9a211fed6daaddffef9767610965
James Seppi <james.seppi@gmail.com>
2015-04-21 15:05:35 -0500

    image upload on google contact form
    closes #136
    also some light refactoring

--- config/config-development.js
+++ config/config-development.js
@@ -6,4 +6,5 @@ angular.module('ConfigApp', [])
   .constant('CONTACT_SUBMIT_URL', 'http://localhost:8001/')
   .constant('CONTACT_UPLOAD_BUCKET', 'contact-uploads')
   .constant('ZIP_UPLOAD_POLICY_URL', 'http://localhost:8001/policy/zip-upload')
+  .constant('IMAGE_UPLOAD_POLICY_URL', 'http://localhost:8001/policy/image-upload')
   ;
